7c2246aeefe86c8d35a346013d3b30f3ca364cba
martinmohan <martinmohan99@gmail.com>
2022-11-08 13:51:56 +0000

    Fixed typo

--- libgosim.go
+++ libgosim.go
@@ -47,7 +47,7 @@ func Ramp(dev string) (float64, error) {
 func main() {
 	//	fmt.Printf("os.Args[0] %s len(os.Args) %d\n",os.Args[0],len(os.Args))
 	if (len(os.Args)<2){
-		fmt.Printf("Usage %s <rampx1 | rampx1 | error | timeout>\n",os.Args[0])
+		fmt.Printf("Usage %s <rampx1 | rampx2 | error | timeout>\n",os.Args[0])
 		os.Exit(0)
 	}
 	for{
