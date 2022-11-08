8abb45bea1143733ff79bd4c5e1c6c7946cabc40
martinmohan <martinmohan99@gmail.com>
2022-11-08 13:04:38 +0000

    Added main() to libgosim

--- libgosim.go
+++ libgosim.go
@@ -1,19 +1,20 @@
-package libgosim
+package main
 
 import (
 	"time"
 	"errors"
 	"fmt"
+	"os"
 )
 
 func Plus(a int, b int) int {
-	    return a + b
+	return a + b
 }
 
 func Plusf(mystring string) (float64, error) {
-		fmt.Printf("In Plusf %s",mystring)
-		var valuef float64 = 1.23
-	    return valuef,nil 
+	fmt.Printf("In Plusf %s",mystring)
+	var valuef float64 = 1.23
+	return valuef,nil 
 }
 
 // Ramp
@@ -30,7 +31,7 @@ func Ramp(dev string) (float64, error) {
 		return valuef*2, nil
 
 		case "timeout": // timeout
-//		time.Sleep(1000)
+		//		time.Sleep(1000)
 		time.Sleep(9999999999999)
 		return valuef, nil
 
@@ -41,4 +42,19 @@ func Ramp(dev string) (float64, error) {
 		return valuef, errors.New("Unknown Device")
 	}
 	return valuef, nil
+}
+
+func main() {
+	//	fmt.Printf("os.Args[0] %s len(os.Args) %d\n",os.Args[0],len(os.Args))
+	if (len(os.Args)<2){
+		fmt.Printf("Usage %s <rampx1 | rampx1 | error | timeout>\n",os.Args[0])
+		os.Exit(0)
+	}
+	for{
+		value,err := Ramp(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("rampx1 %.6f\n",value)
+	}
 }
