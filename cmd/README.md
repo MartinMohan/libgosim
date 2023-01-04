410e6c2eab2e2d769c1a35d343fb4fb0f8501d71
martinmohan <martinmohan99@gmail.com>
2023-01-04 17:12:05 +0000

    Allo ErrorOFF or timeoutOFF

---
+++ cmd/README.md
@@ -0,0 +1,2 @@
+pflag is used instead of flag because you can generate short flags.
+E.g use pflag.StringVarP() instead  of flag.String()
--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
-    "github.com/MartinMohan/libgosim"
+	"github.com/MartinMohan/libgosim"
 	"fmt"
-	"os"
+	"github.com/spf13/pflag"
 )
 
 func main() {
-	//	fmt.Printf("os.Args[0] %s len(os.Args) %d\n",os.Args[0],len(os.Args))
-	if (len(os.Args)<2){
-		fmt.Printf("Usage %s <rampx1 | rampx2 | error | timeout>\n",os.Args[0])
-		os.Exit(0)
+	var mysignal string
+	var ErrorOFF bool
+	var TimeoutOFF bool
+	var noloop bool
+	var status bool
+
+	pflag.StringVarP(&mysignal,"mysignal","m", "rampx1", "<rampx1 | rampx2 | error | timeout>")
+	pflag.BoolVarP(&status,"status","s",false,"Return status")
+	pflag.BoolVarP(&ErrorOFF, "ErrorOFF", "e", false, "Return rampx3 instead of error")
+	pflag.BoolVarP(&TimeoutOFF, "TimeoutOFF", "t", false, "Return rampx4 instead of error")
+	pflag.BoolVarP(&noloop, "noloop", "n", false, "Do not loop result indefinetely")
+	pflag.Parse()
+	//    fmt.Println("sig:", *mysignal)
+	if ErrorOFF {
+		mysignal="error"
+		libgosim.SetErrorOFF(true)
+//		fmt.Printf("ErrorOFF has been set to %v\n",ErrorOFF)
 	}
-	for{
-		value,err := libgosim.Ramp(os.Args[1])
+	if TimeoutOFF {
+		mysignal="timeout"
+		libgosim.SetTimeoutOFF(true)
+//		fmt.Printf("TimeoutOFF has been set to %v\n",TimeoutOFF)
+	}
+	if noloop {
+		value,err := libgosim.Ramp(mysignal)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("%s %.6f\n",os.Args[1],value)
+		fmt.Printf("%s %.6f\n",mysignal,value)
+		return
+	}
+	if status {
+		fmt.Printf("mysignal=%s TimeoutOFF=%v ErrorOFF=%v noloop=%v\n",mysignal,libgosim.GetTimeoutOFF(),libgosim.GetErrorOFF(),noloop)
+		return
+	}
+	// Infinte loop
+	for{
+		value,err := libgosim.Ramp(mysignal)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s %.6f\n",mysignal,value)
 	}
 }
